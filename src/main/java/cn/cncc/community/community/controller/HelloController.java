45881ecd94fad7ed613c0c042d687c9ff879e20a
TomZhangJ <tomcat_cncc@163.com>
2020-01-17 13:21:26 +0800

    add publish

--- src/main/java/cn/cncc/community/community/controller/HelloController.java
+++ src/main/java/cn/cncc/community/community/controller/HelloController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "index";
-    }
+  @GetMapping("/hello")
+  public String hello(@RequestParam(name = "name") String name, Model model) {
+    model.addAttribute("name", name);
+    return "index";
+  }
 }
--- src/main/java/cn/cncc/community/community/controller/IndexController.java
+++ src/main/java/cn/cncc/community/community/controller/IndexController.java
@@ -15,7 +15,6 @@ public class IndexController {
 
   @GetMapping("/")
   public String index(HttpServletRequest request) {
-
     Cookie[] cookies = request.getCookies();
     for (Cookie cookie : cookies) {
       if (cookie.getName().equals("token")) {
