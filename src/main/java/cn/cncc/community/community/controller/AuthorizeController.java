ecfa5dbcef2e3bf89dc2767fffb747d49e2cd15e
TomZhangJ <tomcat_cncc@163.com>
2020-02-19 18:44:01 +0800

    modified logging

--- src/main/java/cn/cncc/community/community/controller/AuthorizeController.java
+++ src/main/java/cn/cncc/community/community/controller/AuthorizeController.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
   @Autowired private GithubProvider githubProvider;
 
@@ -28,8 +30,7 @@ public class AuthorizeController {
   @Value("${github.redirect.uri}")
   private String redirectUri;
 
-  @Autowired
-  private UserService userService;
+  @Autowired private UserService userService;
 
   @GetMapping("/callback")
   public String callback(
@@ -44,9 +45,7 @@ public class AuthorizeController {
     accessTokenDTO.setState(state);
     String accessToken = githubProvider.getAccessToken(accessTokenDTO);
     GithubUser githubUser = githubProvider.getUser(accessToken);
-
-    if (githubUser != null && githubUser.getId() != null)
-    {
+    if (githubUser != null && githubUser.getId() != null) {
       User user = new User();
       String token = UUID.randomUUID().toString();
       user.setToken(token);
@@ -58,20 +57,20 @@ public class AuthorizeController {
       // 登录成功，写cookie 和 session
       // 记录Cookie
       response.addCookie(new Cookie("token", token));
+      return "redirect:/";
     } else {
+      log.error("callback get github error ,{}", githubUser);
       // 登录失败，重新登录
+      return "redirect:/";
     }
-    return "redirect:/";
-    //        return "index";
   }
-  
+
   @GetMapping("/logout")
-  public String logout(HttpServletRequest request,HttpServletResponse response)
-  {
+  public String logout(HttpServletRequest request, HttpServletResponse response) {
     request.getSession().removeAttribute("user");
-     Cookie cookie = new Cookie("token",null);
-     cookie.setMaxAge(0);
-     response.addCookie(cookie);
+    Cookie cookie = new Cookie("token", null);
+    cookie.setMaxAge(0);
+    response.addCookie(cookie);
     return "redirect:/";
   }
 }
